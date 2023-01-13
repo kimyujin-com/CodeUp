e60d407a5087b731b20b432d53ac9e57facf9d90
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2023-01-14 00:13:32 +0900

    Create 1477.c

---
+++ 2차원 배열/1477.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void makeTable(int t[][100], int n, int m)
+{
+	int i, j = 0, k = 1, h, keep = n + m;
+
+	for (h = 0; h <= keep - 2; h++)
+		for (j = m - 1; j >= 0 ; j--)
+			for (i = 0; i < n; i++)
+				if (h == i + j)
+					t[i][j] = k++;
+}
+void printTable(int t[][100], int n, int m)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++)
+			printf("%d ", t[i][j]);
+		printf("\n");
+	}
+}
+int main(void)
+{
+	int n, m;
+	int table[100][100] = { 0 };
+
+	scanf("%d %d", &n, &m);
+
+	makeTable(table, n, m);
+	printTable(table, n, m);
+}
