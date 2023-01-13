a3eb2faed2f734256709d16055bf3f367128320a
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2023-01-14 00:02:43 +0900

    Create 1464.c

---
+++ 2차원 배열/1464.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void makeTable(int t[][100], int n, int m)
+{
+	int i, j, k = 1;
+
+	for (i = n - 1; i >=  0; i--)
+		for (j = m - 1; j >= 0; j--)
+			t[i][j] = k++;
+}
+void printTable(int t[][100], int n, int m)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++)
+			printf("%d ", t[i][j]);
+		printf("\n");
+	}
+}
+int main(void)
+{
+	int n, m;
+	int table[100][100];
+
+	scanf("%d %d", &n, &m);
+
+	makeTable(table, n, m);
+	printTable(table, n, m);
+}
