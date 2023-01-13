168ca0bf5256c96d8090f5d99216fe54282c9b0f
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2023-01-14 00:54:26 +0900

    Create 1501.c

---
+++ 2차원 배열/1501.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void makeTable(int n, int table[][50])
+{
+	int i, j, k = 1;
+
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			table[i][j] = k++;
+}
+
+void printTable(int n, int table[][50])
+{
+	int i, j;
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++)
+			printf("%d ", table[i][j]);
+		printf("\n");
+	}
+}
+
+int main(void)
+{
+	int n, table[50][50];
+
+	scanf("%d", &n);
+	makeTable(n, table);
+	printTable(n, table);
+}
