df3063f6bef88b2e4332e42c728ea1dc14d1117f
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2023-01-14 00:21:41 +0900

    Create 1490.c

---
+++ 2차원 배열/1490.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void printTable(int t[][100], int n, int m)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++)
+			printf("%d ", t[i][j]);
+		printf("\n");
+	}
+
+}
+int main(void)
+{
+	int n, m, i, j;
+	int row, col, cnt = 1, limit = 0, reverse = 1;
+	int t[100][100];
+
+	scanf("%d %d", &n, &m);
+
+	row = n;
+	col = 0;
+	while (cnt <= n * m) {
+		for (i = 0; i < n - limit; i++) {
+			row -= reverse;
+			t[row][col] = cnt++;
+		}
+		for (j = 0; j < m - limit - 1; j++) {
+			col += reverse;
+			t[row][col] = cnt++;
+		}
+		reverse *= -1;
+		limit++;
+	}
+	printTable(t, n, m);
+}
