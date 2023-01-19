d774f238ce3bcfd19f1c57ff4e13123a51e8b2e7
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2023-01-20 01:27:52 +0900

    Create 1912.c

---
+++ 재귀함수/1912.c
@@ -0,0 +1,18 @@
+//n이 입력되면 n!의 값을 출력하시오.
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int factorial(int n)
+{
+	if (n == 1)
+		return 1;
+	else
+		return n * factorial(n - 1);
+}
+int main(void)
+{
+	int n;
+
+	scanf("%d", &n);
+	printf("%d\n", factorial(n));
+}
